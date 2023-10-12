72f382ee574f9b7986af6cfb1639f832e51e4ced
fellipessanha <fellipe900@gmail.com>
2023-10-12 11:58:27 -0300

    chore: lint files

--- include/coordinates.hh
+++ include/coordinates.hh
@@ -1,46 +1,45 @@
 #pragma once
 
-#include <utility>
 #include <ostream>
+#include <utility>
 
 namespace fourier {
 
-template<typename T>
+template <typename T>
 class Coordinates {
-public:
-    std::pair<T, T> coords;
+ public:
+  std::pair<T, T> coords;
 
-    Coordinates& operator+=(const Coordinates& rhs) {
-        coords.first += rhs.coords.first;
-        coords.second += rhs.coords.second;
-        return *this;
-    }
-    friend Coordinates operator+(Coordinates& lhs, const Coordinates& rhs) {
-        lhs+= rhs;
-        return lhs;
-    }
+  Coordinates& operator+=(const Coordinates& rhs) {
+    coords.first += rhs.coords.first;
+    coords.second += rhs.coords.second;
+    return *this;
+  }
+  friend Coordinates operator+(Coordinates& lhs, const Coordinates& rhs) {
+    lhs += rhs;
+    return lhs;
+  }
 
-    Coordinates& operator*=(const double& rhs) {
-        coords.first *= rhs;
-        coords.second += rhs;
-        return *this;
-    }
-    friend Coordinates operator*(Coordinates& lhs, const double& rhs) {
-        lhs*= rhs;
-        return lhs;
-    }
-    friend Coordinates operator*(Coordinates&& lhs, const double& rhs) {
-        lhs*= rhs;
-        return lhs;
-    }
+  Coordinates& operator*=(const double& rhs) {
+    coords.first *= rhs;
+    coords.second += rhs;
+    return *this;
+  }
+  friend Coordinates operator*(Coordinates& lhs, const double& rhs) {
+    lhs *= rhs;
+    return lhs;
+  }
+  friend Coordinates operator*(Coordinates&& lhs, const double& rhs) {
+    lhs *= rhs;
+    return lhs;
+  }
 
-    friend std::ostream& operator<<(std::ostream& out, const Coordinates& c)
-    {
-        return out << '(' << c.coords.first << ", " << c.coords.second << ")";
-    }
+  friend std::ostream& operator<<(std::ostream& out, const Coordinates& c) {
+    return out << '(' << c.coords.first << ", " << c.coords.second << ")";
+  }
 
-    Coordinates() : coords(std::make_pair(0, 0)) {}
-    
-    Coordinates(T x, T y) : coords(std::make_pair(x, y)) {}
+  Coordinates() : coords(std::make_pair(0, 0)) {}
+
+  Coordinates(T x, T y) : coords(std::make_pair(x, y)) {}
 };
-}
+}  // namespace fourier
--- include/fourier_coordinate.hh
+++ include/fourier_coordinate.hh
@@ -1,26 +1,38 @@
 #include "coordinates.hh"
+#include "math.h"
 
 namespace fourier {
 
 class FourierCoordinate {
-    int x_input;
-    double y_input;
-    Coordinates<double> fourier_transformed;
+  int x_input;
+  double y_input;
+  Coordinates<double> fourier_transformed;
 
-    fourier::Coordinates<double> e_alpha_i(double alpha){
-        return Coordinates<double>(cos(alpha), sin(alpha));
-    }
+ public:
+  friend std::ostream& operator<<(std::ostream& out,
+                                  const FourierCoordinate& c) {
+    return out << c.fourier_transformed;
+  }
+  fourier::Coordinates<double> e_alpha_i(double alpha) {
+    return Coordinates<double>(cos(alpha), sin(alpha));
+  }
 
-    Coordinates<double> calculateFourierCoordinates(int x, double y){
-        return e_alpha_i(x) * y;
-    }
+  Coordinates<double> calculateFourierCoordinates(int x, double y) {
+    return e_alpha_i(x) * y;
+  }
 
-    FourierCoordinate(int x, double y) : x_input(x), y_input(y) {
-        fourier_transformed = calculateFourierCoordinates(x, y);
-    }
+  FourierCoordinate(int x, double y) : x_input(x), y_input(y) {
+    fourier_transformed = calculateFourierCoordinates(x, y);
+  }
 
-    FourierCoordinate(std::pair<int, double> i) : x_input(i.first), y_input(i.second) {
-        fourier_transformed = calculateFourierCoordinates(i.first, i.second);
-    }
+  FourierCoordinate(std::pair<int, double> i)
+      : x_input(i.first), y_input(i.second) {
+    fourier_transformed = calculateFourierCoordinates(i.first, i.second);
+  }
+
+  FourierCoordinate(std::pair<int, double>& i)
+      : x_input(i.first), y_input(i.second) {
+    fourier_transformed = calculateFourierCoordinates(i.first, i.second);
+  }
 };
-}
+}  // namespace fourier
