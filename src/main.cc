31a3b1c9b79326106b54a34373825c68f13976f8
fellipessanha <fellipe900@gmail.com>
2023-10-12 12:00:13 -0300

    feat: update main to work with FourerCoordinate

--- src/main.cc
+++ src/main.cc
@@ -1,20 +1,21 @@
 #include <iostream>
 #include <vector>
-#include "coordinates.hh"
+
+#include "fourier_coordinate.hh"
 #include "utils.cc"
 
-int main(){
-    auto vals = fourier::get_2x_sample(11, 10);
-    std::vector<fourier::Coordinates<double>> fouriered;
-    for(auto k: vals){
-        std::cout << "(" << k.first << ", " << k.second << ")\n";
-        auto treated = fourier::e_alpha_i<double>(k.first);
-        fouriered.push_back(treated * k.second);
-    }
+int main() {
+  auto vals = fourier::get_2x_sample(11, 10);
+  std::vector<fourier::FourierCoordinate> fouriered;
+  for (auto k : vals) {
+    std::cout << "(" << k.first << ", " << k.second << ")\nbecomes:\t";
+    fourier::FourierCoordinate treated(k.first, k.second);
+    fouriered.push_back(treated);
+  }
 
-    std::cout << "\n agora fourierizado:\n";
-    for(auto k: fouriered){
-        std::cout << k << "\n";
-    }
-    return 0;
+  std::cout << "\n agora fourierizado:\n";
+  for (auto k : fouriered) {
+    std::cout << k << "\n";
+  }
+  return 0;
 }
