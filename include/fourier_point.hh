c59c82c3450c937ae56d12ad429c6a04d394ff0c
fellipessanha <fellipe900@gmail.com>
2023-10-12 13:44:05 -0300

    feat: add fourier point

---
+++ include/fourier_point.hh
@@ -0,0 +1,29 @@
+#include <vector>
+
+#include "fourier_coordinate.hh"
+
+namespace fourier {
+class FourierPoint {
+  std::vector<FourierCoordinate> coords;
+  Coordinates<double> value;
+  double frequency;
+
+  void calculateFourierPoint() {
+    double x_value = 0, y_value = 0;
+    for (auto v : coords) {
+      x_value += v.x();
+      y_value += v.y();
+    }
+    value = Coordinates<double>(x_value, y_value);
+  }
+
+ public:
+  FourierPoint(std::vector<std::pair<int, double>> inputs, double _frequency)
+      : frequency(_frequency) {
+    for (auto v : inputs) {
+      coords.push_back(FourierCoordinate(v, _frequency));
+    }
+    calculateFourierPoint();
+  }
+};
+}  // namespace fourier
